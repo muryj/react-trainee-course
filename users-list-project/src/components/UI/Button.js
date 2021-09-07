b9753386c48ea9a4ae5712c80fa552825ea59be9
hshchehelskyi <shchehelskyi.hlieb@gmail.com>
2021-09-07 14:19:53 +0300

    Created the UI-component Button, styling for it

---
+++ users-list-project/src/components/UI/Button.js
@@ -0,0 +1,13 @@
+import styles from "./Button.module.css";
+
+export default function Button(props) {
+  return (
+    <button
+      className={styles.button}
+      onClick={props.onClick}
+      type={props.type || "button"}
+    >
+      {props.children}
+    </button>
+  );
+}
