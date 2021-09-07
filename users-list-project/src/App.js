d8f4576de2f713f3d7c48b36e9d37a33d65ac726
hshchehelskyi <shchehelskyi.hlieb@gmail.com>
2021-09-07 14:21:33 +0300

    Added all necessary components to App component

--- users-list-project/src/App.js
+++ users-list-project/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+export default function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, { name: userName, age: userAge }];
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </>
   );
+
 }
 
-export default App;
