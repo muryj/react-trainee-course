3512559339ce40caa779f2b15b043094bcb637fd
hshchehelskyi <shchehelskyi.hlieb@gmail.com>
2021-09-10 19:28:46 +0300

    Added error handling for loading data

--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -7,10 +7,17 @@ import styles from "./AvailableMeals.module.css";
 export default function AvailableMeals(props) {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch("https://react-http-c747f-default-rtdb.europe-west1.firebasedatabase.app/meals.json");
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -27,13 +34,26 @@ export default function AvailableMeals(props) {
       setMeals(loadedMeals);
       setIsLoading(false);
     };
-    fetchMeals();
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+
   }, []);
 
   if (isLoading) {
     return (
       <section className={styles["meals-loading"]}>
         <p>Loading..</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={styles["meals-error"]}>
+        <p>{httpError}</p>
       </section>
     );
   }
