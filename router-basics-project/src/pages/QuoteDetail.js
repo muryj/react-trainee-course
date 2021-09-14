6fb8183bb95aeda66cf52092bc8e041f80d67f5d
hshchehelskyi <shchehelskyi.hlieb@gmail.com>
2021-09-14 21:36:55 +0300

    Added useRouteMatch hook

--- router-basics-project/src/pages/QuoteDetail.js
+++ router-basics-project/src/pages/QuoteDetail.js
@@ -1,4 +1,4 @@
-import { Route, useParams } from "react-router-dom";
+import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
@@ -8,7 +8,10 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
+  const match = useRouteMatch()
   const params = useParams();
+
+  console.log(match);
 
   const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
 
@@ -19,10 +22,17 @@ const QuoteDetail = () => {
   return (
     <>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+
+      <Route path={match.path} exact >
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
-
     </>
   );
 };
